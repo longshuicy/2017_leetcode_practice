38b1f96fa359e355f2f43870fbe21d5794951c96
longshuicy <chenwang.carrie@gmail.com>
2017-02-01 10:14:46 -0600

    should use a collections deque

---
+++ MyStack.py
@@ -0,0 +1,56 @@
+class MyStack(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: void
+        """
+        self.stack.insert(0,x)
+        return self.stack
+        
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        # too ugly!
+        temp = self.stack[0]
+        del self.stack[0]
+        return temp
+        
+
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        return self.stack[0]
+        
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        if not self.stack:
+            return True
+        else:
+            return False
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
