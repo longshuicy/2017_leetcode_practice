e1940bb2042f2a118f22a21fb59d9d2e725faa17
longshuicy <cwang138@illinois.edu>
2017-06-06 20:03:36 -0500

    Create No535.py

---
+++ No535.py
@@ -0,0 +1,30 @@
+class Codec:
+    
+    alphanumeric = string.ascii_letters + '0123456789'
+    
+    def __init__(self):
+        self.url2code = {}
+        self.code2url = {}
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+        
+        :type longUrl: str
+        :rtype: str
+        """
+        
+        while longUrl not in self.url2code:
+            code = ''.join(random.choice(Codec.alphanumeric) for _ in range(6))
+            if code not in self.code2url:
+                self.code2url[code] = longUrl
+                self.url2code[longUrl] = code
+        return 'http://tinyurl.com/' + self.url2code[longUrl]
+        
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+        
+        :type shortUrl: str
+        :rtype: str
+        """
+        
+        return self.code2url[shortUrl][-6:]
