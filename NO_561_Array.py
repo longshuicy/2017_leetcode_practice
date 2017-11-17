a9fda6041b073fd6b44ca19dcebd5ab5ae5be353
longshuicy <cwang138@illinois.edu>
2017-11-17 12:29:52 -0600

    Create NO_561_Array.py

---
+++ NO_561_Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(sorted(nums)[::2])
+    
+# very tricky
+# TIL: [::n] every nth item in the list
