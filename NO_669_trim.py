46d7943b9344f76f1aaea6ad5889953a64688f60
longshuicy <cwang138@illinois.edu>
2017-11-27 13:45:58 -0600

    Create NO_669_trim.py

---
+++ NO_669_trim.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+'''class Solution(object):
+    def trimBST(self, root, L, R):
+        def trim(node):
+            if not node:
+                return None
+            elif node.val > R:
+                return trim(node.left)
+            elif node.val < L:
+                return trim(node.right)
+            else:
+                node.left = trim(node.left)
+                node.right = trim(node.right)
+                return node
+
+        return trim(root)'''
+
+class Solution:
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+        elif root.val >= L and root.val <= R:
+            root.left = self.trimBST(root.left, L, R)
+            root.right = self.trimBST(root.right, L, R)
+            return root
+        elif root.val > R:
+            return self.trimBST(root.left,L,R)
+        elif root.val < L:
+            return self.trimBST(root.right,L,R)
